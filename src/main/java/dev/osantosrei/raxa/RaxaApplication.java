989656d0847eb990a1ab7ed7e8fc9403ee7ba6cc
osantosrei <reinaldo.santos37@outlook.com>
2026-05-13 01:05:51 -0300

    chore: initialize backend project

---
+++ src/main/java/dev/osantosrei/raxa/RaxaApplication.java
@@ -0,0 +1,13 @@
+package dev.osantosrei.raxa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaxaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaxaApplication.class, args);
+	}
+
+}
